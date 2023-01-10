82be86c7c3744d70a002b773f3a4983ccce36298
Connor-Thomson <57526412+connorWT@users.noreply.github.com>
2023-01-10 19:42:35 +0000

    audio

--- HudsonEngine/AudioSystem.cpp
+++ HudsonEngine/AudioSystem.cpp
@@ -4,5 +4,60 @@
 
 using namespace irrklang;
 
-ISoundEngine* SoundEngine = createIrrKlangDevice();
+
+AudioSystem::AudioSystem() 
+{
+    engine = createIrrKlangDevice();
+    volume = 1.0f;
+    currentSound = nullptr;
+}
+
+AudioSystem::~AudioSystem() {
+    engine->drop();
+}
+
+void AudioSystem::playSound(const char* filePath, bool loop) 
+{
+    if (currentSound)
+    {
+        currentSound->stop();
+        currentSound = nullptr;
+    }
+    currentSound = engine->play2D(filePath, loop);
+}
+
+void AudioSystem::pauseSound() 
+{
+    if (currentSound)
+        currentSound->setIsPaused(true);
+}
+
+void AudioSystem::resumeSound() 
+{
+    if (currentSound)
+        currentSound->setIsPaused(false);
+}
+
+void AudioSystem::stopSound() 
+{
+    if (currentSound)
+        currentSound->stop();
+}
+
+void AudioSystem::setVolume(float newVolume)
+{
+    volume = newVolume;
+    engine->setSoundVolume(volume);
+
+}
+
+float AudioSystem::getVolume()
+{
+    return volume;
+}
+
+ISound* AudioSystem::getCurrentSound()
+{
+    return currentSound;
+}
 
--- HudsonEngine/AudioSystem.h
+++ HudsonEngine/AudioSystem.h
@@ -12,40 +12,15 @@ private:
     float volume;
 
 public:
-    AudioSystem() {
-        // Create the audio system
-        engine = createIrrKlangDevice();
-        volume = 2.0f;
-    }
-
-    ~AudioSystem() {
-        // Clean up and shut down the engine
-        engine->drop();
-    }
-
-    void playSound(const char* filePath) {
-       
-        currentSound = engine->play2D(filePath, false, false, true);
-    }
-
-    void pauseSound(const char* filePath) {
-        engine->play2D(filePath, false);
-    }
-
-    void stopSound(const char* filePath) {
-        engine->stopAllSounds();
-    }
-
-    void setVolume(float newVolume)
-    {
-        volume = newVolume;
-        engine->setSoundVolume(volume);
-    }
-
-    float getVolume()
-    {
-        return volume;
-    }
+    AudioSystem();
+    ~AudioSystem();
+    void playSound(const char* filePath, bool loop = false);
+    void pauseSound();
+    void resumeSound();
+    void stopSound();
+    void setVolume(float newVolume);
+    float getVolume();
+    ISound* getCurrentSound();
 };
 
 
