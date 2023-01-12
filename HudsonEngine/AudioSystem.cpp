d99b64c1ebbfca1a68e36ca8ff7e995858084c3e
Connor Thomson <t016482j@student.staffs.ac.uk>
2023-01-12 15:57:22 +0000

    audio
    more working with audio, need to add volume slider to the main application as well as supporting other file formats etc. mp3, wav ....

--- HudsonEngine/AudioSystem.cpp
+++ HudsonEngine/AudioSystem.cpp
@@ -2,17 +2,35 @@
 #include <irrKlang.h>
 #include "AudioSystem.h"
 
+
 using namespace irrklang;
 
+irrklang::ISoundEngine* AudioSystem::engine;
+irrklang::ISound* AudioSystem::music;
 
-AudioSystem::AudioSystem() 
+
+void AudioSystem::init() 
 {
     engine = createIrrKlangDevice();
     volume = 1.0f;
+    pitch = 1.0f;
     currentSound = nullptr;
+    fx = nullptr;
+    loader = nullptr;
+
+    if (!engine)
+    {
+        std::cout << "Engine couldn't be loaded." << std::endl;
+        return; // error starting the audio engine
+    }
+    music = play2D("audio/breakout.mp3", AudioType::music);
+
+    AudioSystem::setVolume();
 }
 
-AudioSystem::~AudioSystem() {
+void AudioSystem::deinit() 
+{
+    music->drop();
     engine->drop();
 }
 
@@ -38,16 +56,35 @@ void AudioSystem::resumeSound()
         currentSound->setIsPaused(false);
 }
 
-void AudioSystem::stopSound() 
+void AudioSystem::stopSound() //stops audio immediately during playback
 {
     if (currentSound)
         currentSound->stop();
 }
 
-void AudioSystem::setVolume(float newVolume)
+void AudioSystem::removeAllSounds()  // Removes all sound sources
 {
-    volume = newVolume;
-    engine->setSoundVolume(volume);
+    if (currentSound)
+        engine->removeAllSoundSources();
+}
+
+void AudioSystem::soundPitch(float newPitch)
+{
+    pitch = newPitch;
+
+    if (currentSound)
+        currentSound->setPlaybackSpeed();
+}
+
+void AudioSystem::soundPanning()
+{
+
+}
+void AudioSystem::setVolume()
+{
+    AudioSystem::engine->setSoundVolume();
+    music->setVolume();
+   
 
 }
 
@@ -58,6 +95,51 @@ float AudioSystem::getVolume()
 
 ISound* AudioSystem::getCurrentSound()
 {
+    return currentSound;
+}
+
+ISoundEffectControl* AudioSystem::soundEffect()
+{
+    currentSound = engine->play2D("sound.wav", true, false, true, ESM_AUTO_DETECT, true);
+
+    if (currentSound)
+    {
+        fx = currentSound->getSoundEffectControl();
+        if (fx)
+        {
+            fx->enableEchoSoundEffect();
+        }
+    }
+    currentSound->drop();
+}
+
+ISound* AudioSystem::play2D(std::string musicFile, AudioType audiotype, bool playLooped)
+{
+    ISound* currentSound = engine->play2D(musicFile.c_str(), playLooped, false, true);
+    if (!currentSound)
+    {
+        std::cout << "Sound cannot be played" + musicFile << std::endl;
+        return nullptr;
+    }
+
+    switch (audiotype)
+    {
+        case AudioType::soundeffect:
+        {
+        currentSound->setVolume();
+        break;
+        }
+        case AudioType::ambient:
+        {
+        currentSound->setVolume();
+        break;
+        }
+        case AudioType::music:
+        {
+        currentSound->setVolume();
+        break;
+        }
+    }
     return currentSound;
 }
 
--- HudsonEngine/AudioSystem.h
+++ HudsonEngine/AudioSystem.h
@@ -1,26 +1,53 @@
 #pragma once
 #include <irrKlang.h>
+#include <string>
 
 using namespace irrklang;
 
-class AudioSystem
+
+static class AudioSystem
 {
+    enum AudioType
+    {
+        soundeffect,
+        ambient,
+        music,
+    };
 
 private:
-	ISoundEngine* engine;
-    ISound* currentSound;
-    float volume;
+	static irrklang::ISoundEngine* engine;
+    static irrklang::ISoundEffectControl* fx;
+    static irrklang::IAudioStreamLoader* loader;
+    static irrklang::ISound* currentSound;
+    static irrklang::ISound* music;
+    static float volume;
+    static float pitch;
 
 public:
-    AudioSystem();
-    ~AudioSystem();
-    void playSound(const char* filePath, bool loop = false);
-    void pauseSound();
-    void resumeSound();
-    void stopSound();
-    void setVolume(float newVolume);
-    float getVolume();
-    ISound* getCurrentSound();
+    static void init();
+    static void deinit();
+
+    static void playSound(const char* filePath, bool loop = false);
+    static void pauseSound();
+
+    static void resumeSound();
+    static void stopSound();
+
+    static void removeAllSounds();
+
+    static void soundPitch(float newPitch);
+    static void soundPanning();
+
+    static void updateAudioListener();
+
+
+    static void setVolume();
+    static float getVolume();
+    static irrklang::ISound* getCurrentSound();
+    static irrklang::ISoundEffectControl* soundEffect();
+    static irrklang::ISound* play2D(std::string musicFile, AudioType audiotupe = AudioType::soundeffect, bool playLooped = false);
+    
+
 };
 
 
