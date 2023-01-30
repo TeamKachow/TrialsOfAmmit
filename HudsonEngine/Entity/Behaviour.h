7d20ba136a11e3ca0223664905e3cfc6f572b23b
Mathew Dacosta <72082814+mathewdacosta@users.noreply.github.com>
2023-01-30 12:40:28 +0000

    Make Behaviour create/destroy hooks optional

--- HudsonEngine/Entity/Behaviour.h
+++ HudsonEngine/Entity/Behaviour.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include "Common.h"
 #include "Component.h"
 
@@ -21,15 +21,15 @@ namespace Hudson::Entity
         /**
          * \brief Handles when a behaviour is added to a game object.
          */
-        virtual void OnCreate() = 0;
+        virtual void OnCreate() {}
         /**
          * \brief Handles when a game object's behaviours are updated.
          * \param dt The interval sicne the last update loop. (This interval should not change at runtime.)
          */
-        virtual void OnTick(const double &dt) = 0;
+        virtual void OnTick(const double& dt) = 0;
         /**
          * \brief Handles when the behaviour is removed from its game object.
          */
-        virtual void OnDestroy() = 0;
+        virtual void OnDestroy() {}
     };
 }
