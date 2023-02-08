6c58f73bba7b6801958a5f0150576c146866882f
Kehinde Tinubu <71414789+kehindetinubu@users.noreply.github.com>
2023-02-08 20:43:17 +0000

    slight fuck up with one up...
    the one-up that works is the commented one

--- TrialsOfAmmit/OneUP.cpp
+++ TrialsOfAmmit/OneUP.cpp
@@ -3,7 +3,7 @@
 OneUP::OneUP()
 {
 	_abilityActiveTime = 10;
-	_abilityCoolDownTime = 50;
+	_abilityCoolDownTime = 5;
 	_abilityState = ready;
 
 	_healthRegained = 0.5; // as in 50%
@@ -24,27 +24,41 @@ void OneUP::UseAbility(Hudson::World::Scene* _CurrentPassScene)
 	{
 		if (other->GetName() == "Player")
 		{
-			_player = other->GetComponent<Player>();
-			// INSERT CODE HERE . . .
-			if (_player->_playerHealth < 1)
+
+			std::cout << " Ability OneUP is active ! \n" ;
+			
+			/*_player = other->GetComponent<Player>();
+			if (_player->_playerHealth < 30)
 			{
+				std::cout << " Player is gon die !!! \n";
 				_playerDead = true;
-			}
-			/*else
-			{
-				_abilityState = ready;
 			}*/
 
-			if (_playerDead)
+			/*if (_playerDead)
 			{
+				std::cout << " Ability OneUP is activated !!! \n";
 				_player->_playerHealth = _player->_maxHealth * _healthRegained;
 				DeactivateAbility(_CurrentPassScene);
+			}*/
+			
+			if (_player->_playerHealth < 60)
+			{
+				_playerDead = true;
+
+				if (_playerDead)
+				{
+					std::cout << " Ability OneUP is activated !!! \n";
+					_player->_playerHealth = _player->_maxHealth * _healthRegained;
+					DeactivateAbility(_CurrentPassScene);
+				}
 			}
 
+
+
 			break;
 		}
 	};
-
+	//std::cout << " Ability OneUP is active ! \n" ;
 	_abilityState = active;
 }
 
@@ -66,53 +80,4 @@ void OneUP::DeactivateAbility(Hudson::World::Scene* _CurrentPassScene)
 
 	_abilityState = cooldown;
 }
-/*
-OneUP::OneUP()
-{
-	_abilityActiveTime = 10;
-	_abilityCoolDownTime = 50;
-	_abilityState = ready;
-}
 
-OneUP::~OneUP()
-{
-}
-
-void OneUP::UseAbility(Hudson::World::Scene* _CurrentPassScene)
-{
-	Hudson::World::Scene* _currentScene;
-	_currentScene = _CurrentPassScene;
-	auto _sceneObjects = _currentScene->GetObjects();
-	for (Hudson::Entity::GameObject* other : _sceneObjects)
-	{
-		if (other->GetName() == "Player")
-		{
-			_player = other->GetComponent<Player>();
-			// INSERT CODE HERE . . .
-
-			break;
-		}
-	};
-
-	_abilityState = active;
-}
-
-void OneUP::DeactivateAbility(Hudson::World::Scene* _CurrentPassScene)
-{
-	Hudson::World::Scene* _currentScene;
-	_currentScene = _CurrentPassScene;
-	auto _sceneObjects = _currentScene->GetObjects();
-	for (Hudson::Entity::GameObject* other : _sceneObjects)
-	{
-		if (other->GetName() == "Player")
-		{
-			_player = other->GetComponent<Player>();
-			// INSERT CODE HERE . . .
-
-			break;
-		}
-	};
-
-	_abilityState = cooldown;
-}
-*/
